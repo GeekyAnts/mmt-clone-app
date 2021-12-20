8d103506b33258e76b037e2dd39a1c2843635da2
sankalp <sankalp@geekyants.com>
2021-12-20 20:06:13 +0530

    flight list

---
+++ components/FlightList.tsx
@@ -0,0 +1,187 @@
+import React, { useState } from "react";
+import { Box, HStack, Link, Text, VStack } from "native-base";
+
+import Image from "next/image";
+const List = [
+  {
+    imageOne: "/images/hotel1.jpeg",
+    place: "Chennai Flights",
+    subText: "Mumbai,Delhi",
+    via: "via-",
+  },
+  {
+    imageOne: "/images/hotel1.jpeg",
+    place: "Goa Flights",
+    subText: "Delhi,Varanasi",
+    via: "via-",
+  },
+  {
+    imageOne: "/images/hotel1.jpeg",
+    place: "Mumbai Flights",
+    subText: "Bangalore",
+    via: "via-",
+  },
+];
+const List2 = [
+  {
+    imageOne: "/images/hotel1.jpeg",
+    place: "Pune Flights",
+    subText: "Hyderabad, Goa",
+    via: "via-",
+  },
+  {
+    imageOne: "/images/hotel1.jpeg",
+    place: "Delhi Flights",
+    subText: "Patna,Delhi",
+    via: "via-",
+  },
+  {
+    imageOne: "/images/hotel1.jpeg",
+    place: "Pune Flights",
+    subText: "Delhi",
+    via: "via-",
+  },
+];
+const List3 = [
+  {
+    imageOne: "/images/hotel1.jpeg",
+    place: "Patna Flights",
+    subText: "Mumbai",
+    via: "via-",
+  },
+  {
+    imageOne: "/images/hotel1.jpeg",
+    place: "Bangalore Flights",
+    subText: "Jaunpur,Delhi",
+    via: "via-",
+  },
+  {
+    imageOne: "/images/hotel1.jpeg",
+    place: "Jaipur Flights",
+    subText: "Patna,Varanasi",
+    via: "via-",
+  },
+];
+export default function FlightList(props: any) {
+  return (
+    <Box shadow={5} mx={40} py={5} bg="white" borderRadius={10}>
+      <HStack justifyContent="space-evenly">
+        {List.map((item, index) => {
+          return (
+            <VStack key={index}>
+              <HStack alignItems="center" space={4}>
+                <Image
+                  src={item.imageOne}
+                  alt="Vercel Logo"
+                  width={64}
+                  height={64}
+                  // @ts-ignore
+                  borderRadius={20}
+                  rounded="full"
+                />
+                <VStack>
+                  <Text
+                    fontWeight="semibold"
+                    color="coolGray.800"
+                    fontSize="md"
+                  >
+                    {item.place}
+                  </Text>
+                  <Link
+                    href="https://nativebase.io"
+                    _text={{
+                      color: "coolGray.800",
+                      fontSize: "xs",
+                      fontWeight: "bold",
+                      textDecorationLine: "none",
+                    }}
+                  >
+                    {item.subText}
+                  </Link>
+                </VStack>
+              </HStack>
+            </VStack>
+          );
+        })}
+      </HStack>
+      <HStack justifyContent="space-evenly" mt={2}>
+        {List2.map((item, index) => {
+          return (
+            <VStack key={index}>
+              <HStack alignItems="center" space={4}>
+                <Image
+                  src={item.imageOne}
+                  alt="Vercel Logo"
+                  width={64}
+                  height={64}
+                  // @ts-ignore
+                  borderRadius={20}
+                  rounded="full"
+                />
+                <VStack>
+                  <Text
+                    fontWeight="semibold"
+                    color="coolGray.800"
+                    fontSize="md"
+                  >
+                    {item.place}
+                  </Text>
+                  <Link
+                    href="https://nativebase.io"
+                    _text={{
+                      color: "coolGray.800",
+                      fontSize: "xs",
+                      fontWeight: "bold",
+                      textDecorationLine: "none",
+                    }}
+                  >
+                    {item.subText}
+                  </Link>
+                </VStack>
+              </HStack>
+            </VStack>
+          );
+        })}
+      </HStack>
+      <HStack justifyContent="space-evenly" mt={2}>
+        {List3.map((item, index) => {
+          return (
+            <VStack key={index}>
+              <HStack alignItems="center" space={4}>
+                <Image
+                  src={item.imageOne}
+                  alt="Vercel Logo"
+                  width={64}
+                  height={64}
+                  // @ts-ignore
+                  borderRadius={20}
+                  rounded="full"
+                />
+                <VStack>
+                  <Text
+                    fontWeight="semibold"
+                    color="coolGray.800"
+                    fontSize="md"
+                  >
+                    {item.place}
+                  </Text>
+                  <Link
+                    href="https://nativebase.io"
+                    _text={{
+                      color: "coolGray.800",
+                      fontSize: "xs",
+                      fontWeight: "bold",
+                      textDecorationLine: "none",
+                    }}
+                  >
+                    {item.subText}
+                  </Link>
+                </VStack>
+              </HStack>
+            </VStack>
+          );
+        })}
+      </HStack>
+    </Box>
+  );
+}
