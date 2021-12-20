fe9abf0ed1a8ccef81a6990f113f0cf1d2bc5060
sankalp <sankalp@geekyants.com>
2021-12-20 22:55:21 +0530

    made explore component

---
+++ components/Explore.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import {
+  Box,
+  Divider,
+  HStack,
+  Icon,
+  Pressable,
+  Text,
+  VStack,
+} from "native-base";
+import {
+  Entypo,
+  FontAwesome,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from "@expo/vector-icons";
+const List = [
+  {
+    iconName: "road",
+    iconText: "Trip Ideas",
+    subText: false,
+    as: MaterialCommunityIcons,
+  },
+  {
+    iconName: "image",
+    iconText: "Trip Money",
+    iconSubText: "Loans Credit & More",
+    as: Entypo,
+  },
+  {
+    iconName: "map-marker",
+    iconText: "Nearby Getaways",
+    subText: false,
+    as: FontAwesome,
+  },
+  {
+    iconName: "card-giftcard",
+    iconText: "Gift Cards",
+    subText: false,
+    as: MaterialIcons,
+  },
+];
+
+export default function Explore(props: any) {
+  return (
+    <Box
+      bg="white"
+      shadow={6}
+      rounded="full"
+      px={5}
+      py={3}
+      justifyContent="center"
+      mx={40}
+    >
+      <Pressable
+        // @ts-ignore
+        onPress={() => {
+          console.log("hello");
+        }}
+      >
+        <HStack alignItems="center" justifyContent="space-evenly">
+          {List.map((item, index) => {
+            return (
+              <HStack key={index} alignItems="center" space={2}>
+                <Icon as={item.as} name={item.iconName} color="#1E93FD" />
+                {item.subText == false ? (
+                  <Text fontSize="sm" color="coolGray.800" fontWeight="medium">
+                    {item.iconText}
+                  </Text>
+                ) : (
+                  <VStack>
+                    <Text
+                      fontSize="sm"
+                      color="coolGray.800"
+                      fontWeight="medium"
+                    >
+                      {item.iconText}
+                    </Text>
+                    <Text
+                      fontSize={10}
+                      color="coolGray.400"
+                      fontWeight="normal"
+                    >
+                      {item.iconSubText}
+                    </Text>
+                  </VStack>
+                )}
+                <Divider orientation="vertical" bg="red.500" />
+              </HStack>
+            );
+          })}
+        </HStack>
+      </Pressable>
+    </Box>
+  );
+}
