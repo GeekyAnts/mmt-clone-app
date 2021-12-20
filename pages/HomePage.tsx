a68592bcebf59f2775acd6c8965b1adf0787ea14
Abhineet Kumar <ankitsagarsingh@Abhineets-MacBook-ProG0124.local>
2021-12-20 19:35:10 +0530

    worked on the homepage screen

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -24,6 +24,13 @@ import Header from "../components/Header";
 
 //   colors: ["#091B30", "#FEFBF4"],
 
+const fareType = [
+  { type: "Regular Fares" },
+  { type: "Armed Forces Fares" },
+  { type: "Student Fares" },
+  { type: "Senior Citizen Fares" },
+  { type: "Double Seat Fares" },
+];
 const config = {
   dependencies: {
     "linear-gradient": require("expo-linear-gradient").LinearGradient,
@@ -112,23 +119,136 @@ export default function HomePage() {
                     MULTICITY
                   </Radio>
                 </Radio.Group>
-                <HStack>
-                  <Text fontWeight="bold" fontSize="xs">
-                    International Flights
+                <HStack justifyContent="center" alignItems="center" pt={10}>
+                  <Text fontWeight="bold" fontSize="xs" textAlign="center">
+                    International Flights |
                   </Text>
-                  <Divider orientation="vertical" width="1"></Divider>
-                  <Text fontWeight="bold" fontSize="xs">
-                    International Flights
+
+                  <Text fontWeight="bold" fontSize="xs" color="#4A4A4A">
+                    {" "}
+                    Domestic Flights |
                   </Text>
-                  <Divider orientation="vertical" width="1"></Divider>
                 </HStack>
               </HStack>
+              <Box
+                borderRadius="lg"
+                borderWidth="1"
+                borderColor="coolGray.500"
+                px="5"
+              >
+                <HStack width="100%">
+                  <HStack width="50%">
+                    <VStack>
+                      <Text>FROM</Text>
+                      <Text>Delhi</Text>
+                      <Text>DEL,Delhi Airport India</Text>
+                    </VStack>
+                    <VStack>
+                      <Text>TO</Text>
+                      <Text>Banglore</Text>
+                      <Text>BLR, Kempegowda International Airport India</Text>
+                    </VStack>
+                  </HStack>
+                  <HStack width="50%">
+                    <VStack width="30%">
+                      <HStack>
+                        <Text>DEPARTURE</Text>
+                        <Icon
+                          size="md"
+                          as={AntDesign}
+                          name="arrowright"
+                          color="white"
+                        />
+                      </HStack>
+                      <HStack>
+                        {" "}
+                        <Text>9 </Text> <Text>Jan'22</Text>
+                      </HStack>
+                      <Text>Sunday</Text>
+                    </VStack>
+                    <VStack width="30%">
+                      <HStack>
+                        <Text>DEPARTURE</Text>
+                        <Icon
+                          size="md"
+                          as={AntDesign}
+                          name="arrowright"
+                          color="white"
+                        />
+                      </HStack>
+
+                      <Text lineHeight="16">
+                        Tap to add a return date for bigger discounts
+                      </Text>
+                    </VStack>
+                    <VStack width="40%">
+                      <HStack>
+                        <Text>TRAVELLERS &amp; CLASS</Text>
+                        <Icon
+                          size="md"
+                          as={AntDesign}
+                          name="arrowright"
+                          color="white"
+                        />
+                      </HStack>
+                      <HStack>
+                        {" "}
+                        <Text>9 </Text> <Text>Jan'22</Text>
+                      </HStack>
+                      <Text>Sunday</Text>
+                    </VStack>
+                  </HStack>
+                </HStack>
+              </Box>
+              <HStack justifyContent="space-between">
+                <Radio.Group
+                  defaultValue="1"
+                  size="sm"
+                  name="exampleGroup"
+                  flexDirection="row"
+                >
+                  {fareType.map((item, index) => {
+                    return (
+                      <Box pt={10} width="12%">
+                        <Radio
+                          _text={{
+                            mx: 2,
+                            fontWeight: "bold",
+                            fontSize: "xs",
+                          }}
+                          colorScheme="green"
+                          value="1"
+                          icon={
+                            <Icon
+                              as={<MaterialCommunityIcons name="alien" />}
+                            />
+                          }
+                          my={1}
+                        >
+                          {item.type}
+                        </Radio>
+                      </Box>
+                    );
+                  })}
+                </Radio.Group>
+
+                <HStack justifyContent="center" alignItems="center" pt={10}>
+                  <Text fontWeight="bold" fontSize="xs" textAlign="center">
+                    Trending Searches:
+                  </Text>
+                  <HStack>
+                    <Text fontWeight="bold" fontSize="xs" color="#4A4A4A">
+                      {" "}
+                      Banglore
+                    </Text>
+                    <Icon as={<MaterialCommunityIcons name="alien" />} />
+                    {/* <Icon as="MaterialCommunityIcons" name="alien" /> */}
+                  </HStack>
+                </HStack>
+              </HStack>
+              {/* </Box> */}
             </VStack>
-            {/* <Header /> */}
           </Box>
-          {/* {tabName == "Login" ? <OrderInit /> : null}
-        {tabName == "Address" ? <OrderAddress /> : null}
-        {tabName == "Payment" ? <OrderPayment /> : null} */}
         </VStack>
       </Box>
     </NativeBaseProvider>
