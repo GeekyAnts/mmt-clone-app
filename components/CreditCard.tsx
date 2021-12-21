b07d6bf4c3f12d666e2cafc91f579e0814e2c106
sankalp <sankalp@geekyants.com>
2021-12-21 09:47:22 +0530

    credit card component completed

---
+++ components/CreditCard.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Box, HStack, Icon, Pressable, Text, VStack } from "native-base";
+import Image from "next/image";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const List1 = [
+  {
+    imageOne: "/images/trip.png",
+    imageTwo: "/images/card.png",
+    imageText: "Credit Card",
+    subtext: "Compare & apply for the best credit cards in India.",
+    iconName: "arrow-right-alt",
+    as: MaterialIcons,
+    borderçolor: "#F1F199",
+  },
+  {
+    imageOne: "/images/trip.png",
+    imageTwo: "/images/person.png",
+    imageText: "Travel Loan",
+    subtext: "Get upto ₹1 lakh limit & pay back in flexible EMIs.",
+    iconName: "arrow-right-alt",
+    as: MaterialIcons,
+    borderçolor: "#FFCBCA",
+  },
+  {
+    imageOne: "/images/trip.png",
+    imageTwo: "/images/person2.png",
+    imageText: "Personal Loan",
+    subtext: "Get approval for upto ₹30,00,000 at low interest rates.",
+    iconName: "arrow-right-alt",
+    as: MaterialIcons,
+    borderçolor: "#3AE5E0",
+  },
+];
+
+export default function CreditCard(props: any) {
+  return (
+    <HStack alignItems="center" mx={40} space={4}>
+      {List1.map((item, index) => {
+        return (
+          <Pressable
+            key={index}
+            // @ts-ignore
+            onPress={() => {
+              console.log("hello");
+            }}
+          >
+            <Box
+              borderColor={item.borderçolor}
+              borderTopWidth={1}
+              borderRightWidth={1}
+              borderBottomWidth={1}
+              borderLeftWidth={32}
+            >
+              <HStack alignItems="center" space={3}>
+                <Image
+                  src={item.imageTwo}
+                  // @ts-ignore
+                  zIndex={1}
+                  alt="Vercel Logo"
+                  width={48}
+                  height={48}
+                  // @ts-ignore
+                />
+                <VStack space={1}>
+                  <Text
+                    color="coolGray.800"
+                    fontWeight="semibold"
+                    fontSize="sm"
+                  >
+                    {item.imageText}
+                  </Text>
+                  <Text color="coolGray.400" fontWeight="normal" fontSize="xs">
+                    {item.subtext}
+                  </Text>
+                </VStack>
+                <Icon
+                  as={item.as}
+                  name={item.iconName}
+                  color="#008BFF"
+                  size={7}
+                />
+              </HStack>
+            </Box>
+          </Pressable>
+        );
+      })}
+    </HStack>
+  );
+}
