78548acf4cbe42be2095eccc44050dd563b1a3ed
sankalp <sankalp@geekyants.com>
2022-01-04 19:55:58 +0530

    made the tab component

--- components/Download.tsx
+++ components/Download.tsx
@@ -1,36 +1,18 @@
-import React, { useState, useMemo } from "react";
+import React from "react";
 import {
   Box,
   Divider,
   HStack,
-  Icon,
-  Link,
   Text,
   VStack,
   Pressable,
   Input,
 } from "native-base";
-import {
-  Entypo,
-  FontAwesome5,
-  MaterialCommunityIcons,
-  MaterialIcons,
-  SimpleLineIcons,
-} from "@expo/vector-icons";
 import Image from "next/image";
 
 export default function Download(props: any) {
   return (
-    <Box
-      bg="white"
-      py="5"
-      w="100%"
-      shadow="4"
-      mt={4}
-      // bg="red.100"
-      pl={6}
-      //   alignItems={"center"}
-    >
+    <Box bg="white" py="5" w="100%" shadow="4" mt={4} pl={6}>
       <Text color="black" fontWeight="extrabold" fontSize="4xl">
         Download App Now !
       </Text>
@@ -43,17 +25,11 @@ export default function Download(props: any) {
           alignItems="flex-start"
           alignContent="flex-start"
           alignSelf=""
-          // bg="red.900"
         >
           <Text fontSize="xs" color="coolGray.800" fontWeight="semibold">
             8% Off upto Rs 5000 use coupon code WELCOMEMMT on APP
           </Text>
           <Input
-            // w={{
-            //   base: "75%",
-            //   md: "25%",
-
-            // }}
             placeholder="Enter Mobile Number"
             InputLeftElement={
               <HStack space="2" pl="2">
@@ -91,29 +67,12 @@ export default function Download(props: any) {
             MORE WAYS TO GET THE APP
           </Text>
           <HStack space={3}>
-            {/* <VStack space={2}> */}
             <Image
               src="/images/image_store.png"
               alt="vercel logo"
-              // layout="fill"
               width={120}
               height={48}
             />
-            {/* <Image
-                src="/images/appstore.png"
-                alt="Vercel Logo"
-                // layout="fill"
-                width={96}
-                height={48}
-              />
-              <Image
-                src="/images/playstore.png"
-                alt="Vercel Logo"
-                // layout="fill"
-                width={96}
-                height={72}
-              /> */}
-            {/* </VStack> */}
             <VStack alignItems="center">
               <Image
                 src="/images/qrcode.png"
